ea2f6d2904d511073e7e7be8c7e6cc16b55513d6
Jethet <henriette.hettinga@gmail.com>
2020-01-07 15:41:17 +0100

    Using IIFEs

--- JS_SANDBOX/jsFunctions.js
+++ JS_SANDBOX/jsFunctions.js
@@ -5,3 +5,14 @@ function countTwo(x = 5, y = 1){  // 5 and 7 are default values
 }
 console.log(countTwo(12, 17)); // = 29
 console.log(countTwo());  // No parameters given, so default parameters: = 6
+
+// IMMEDIATELY INVOKABLE FUNCTION EXPRESSIONS: IIFEs
+// An IIFE is a function that is declared and run at the same time
+
+(function(){
+    console.log('IIFE Ran ....');
+})();  // Parentheses are needed because this is a function !
+
+(function(name){
+    console.log('Hello ' + name);
+})('default');
