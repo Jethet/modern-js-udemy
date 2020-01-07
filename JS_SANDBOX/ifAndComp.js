1a3419bf5e8a3520731a3699b78de9197c8e8eb3
Jethet <henriette.hettinga@gmail.com>
2020-01-07 15:29:39 +0100

    Example of switches

--- JS_SANDBOX/ifAndComp.js
+++ JS_SANDBOX/ifAndComp.js
@@ -4,3 +4,29 @@ const id = 108;
 
 console.log(id ===100 ? 'CORRECT' : 'INCORRECT'); // = incorrect
 
+let day;
+
+switch(new Date().getDay()){
+    case 0:     // Numerical for Sunday: week starts at Sun = 0
+        day = 'Sunday';
+        break;
+    case 1:
+        day = 'Monday';
+        break;
+    case 2:
+        day = 'Tuesday';
+        break;
+    case 3:
+        day = 'Wednesday';
+        break;
+    case 1:
+        day = 'THursday';
+        break;
+    case 1:
+        day = 'Friday';
+        break;
+    case 1:
+        day = 'Saturday';
+     break;
+}
+console.log(`Today is ${day}`);
