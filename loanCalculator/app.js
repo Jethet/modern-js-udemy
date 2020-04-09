976700e547fc27be0bf36f3ba00b02a49d046edb
Jethet <henriette.hettinga@gmail.com>
2020-04-09 18:58:03 +0200

    Add html and js code for project loan calculator; add image

---
+++ loanCalculator/app.js
@@ -0,0 +1,31 @@
+// Listen for submit
+
+document.querySelector("#loan-form").addEventListener("submit", calculateResults);
+
+// Calculate results
+function calculateResults(e) {
+  // UI variables: it can be useful to add UI to the variable name, to make this clear
+  const amount = document.querySelector("#amount");
+  const interest = document.querySelector("#interest");
+  const years = document.querySelector("#years");
+  const monthlyPayment = document.querySelector("#monthly-payment");
+  const totalPayment = document.querySelector("#total-payment");
+  const totalInterest = document.querySelector("#total-interest");
+
+  const principal = parseFloat(amount.value); // in the US, 'principal' is the loan amount
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+  // Compute monthly payments
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+  } else {
+    console.log('Please check your numbers');
+  }
+  e.preventDefault();
+}
