0782fa0a5f22b3bb591af82c31891e9dc906db23
Jethet <henriette.hettinga@gmail.com>
2020-04-09 19:20:58 +0200

    Add formulas for calculation; escape error with emptying fields by using 'autocomplete=off' in html input

--- loanCalculator/app.js
+++ loanCalculator/app.js
@@ -23,9 +23,35 @@ function calculateResults(e) {
   if (isFinite(monthly)) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
-    totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+    totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
   } else {
-    console.log('Please check your numbers');
+    showError("Please check your numbers");
   }
   e.preventDefault();
+}
+
+// Show error message
+function showError(error) {
+  // Create a div
+  const errorDiv = document.createElement("div");
+
+  // Get elements
+  const card = document.querySelector(".card");
+  const heading = document.querySelector(".heading");
+
+  // Add class to div
+  errorDiv.className = "alert alert-danger";
+
+  // Create text node and append to div
+  errorDiv.appendChild(document.createTextNode(error));
+
+  // Insert error above heading
+  card.insertBefore(errorDiv, heading);
+
+  // Clear error message after 3 seconds
+  setTimeout(clearError, 3000);
+}
+
+function clearError() {
+  document.querySelector(".alert").remove();
 }
