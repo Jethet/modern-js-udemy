63d1e86c8e39010e50e0b4a0b45588b117aef796
Jethet <henriette.hettinga@gmail.com>
2020-04-10 10:37:07 +0200

    Use skeleton for HTML setup; define game function

---
+++ numberGuesser/app.js
@@ -0,0 +1,9 @@
+/* Game function:
+- player can guess a number between a min and max
+- player gets a number of guesses
+- player is notified of remaining number of guesses
+- player is notified of correct answer when player loses
+- player is notified when answer is correct
+- player can play again
+*/
+
