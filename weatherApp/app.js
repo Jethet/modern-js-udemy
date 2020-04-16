ef41c9a0be708ca2a44a0e38a08f25430b3d1c10
Jethet <henriette.hettinga@gmail.com>
2020-04-16 17:54:17 +0200

    Create weather class and getWeather function, returns weather object

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -1,3 +1,16 @@
 // For API token, see .env file in GitHubFinder project
 
+// Initialize weather object
+const weather = new Weather("Amsterdam", "NL");
 
+// Get weather on DOM load
+document.addEventListener('DOMContentLoaded', getWeather)
+
+// This function must be called when the DOM loads, see eventlistener above
+function getWeather() {
+weather.getWeather() // getWeather is async (see weather.js) returns a promise
+  .then((results) => {
+    console.log(results);
+  })
+  .catch((err) => console.log(err));
+}
---
+++ weatherApp/weather.js
@@ -0,0 +1,24 @@
+class Weather {
+  constructor(city, country) {
+    this.apiKey = "";
+    this.city = city;
+    this.country = country;
+  }
+
+  // Fetch weather from API
+  async getWeather() {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&appid=${this.apiKey}&units=metric`
+    );
+    const responseData = await response.json();
+    console.log(responseData);
+    
+    return responseData.main;
+  }
+
+  // Change weather location
+  changeLocation(city, country) {
+    this.city = city;
+    this.country = country;
+  }
+}
