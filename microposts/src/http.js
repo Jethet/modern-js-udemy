9784a84d4b34a3fc86a841c79a27646866aebd92
Jethet <henriette.hettinga@gmail.com>
2020-04-25 18:17:08 +0200

    Create http module and ui module; code to insert posts into ui; add icons

---
+++ microposts/src/http.js
@@ -0,0 +1,49 @@
+class EasyHTTP {
+  // Make HTTP GET request with async/await
+  async get(url) {
+    const response = await fetch(url);
+    const resData = await response.json();
+    return resData;
+  }
+
+  // Make POST request with async/await
+  async post(url, data) {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    const resData = await response.json();
+    return resData;
+  }
+
+  // Make PUT request with async/await
+  async put(url, data) {
+    const response = await fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const resData = await response.json()
+    return resData
+  }
+
+  // Make DELETE request with async/await
+  async delete(url) {
+    const response = await fetch(url, {
+      method: 'DELETE',
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+    const resData = await 'Resource deleted'
+    return resData
+  }
+}
+
+export const http = new EasyHTTP()
