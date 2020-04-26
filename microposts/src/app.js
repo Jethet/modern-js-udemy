54543bd33200510caf29223dea806e7d0c3575be
Jethet <henriette.hettinga@gmail.com>
2020-04-26 12:03:09 +0200

    Add new posts, create alert with timeout

--- microposts/src/app.js
+++ microposts/src/app.js
@@ -1,11 +1,37 @@
-import { http } from './http'
-import { ui } from './ui'
+import { http } from "./http";
+import { ui } from "./ui";
 
 // Get posts on DOM load
-document.addEventListener('DOMContentLoaded', getPosts)
+document.addEventListener("DOMContentLoaded", getPosts);
 
+// Listen for add post
+document.querySelector(".post-submit").addEventListener("click", submitPost);
+
+// Get posts
 function getPosts() {
-  http.get('http://localhost:3000/posts')
-  .then(data => ui.showPosts(data))
-  .catch(err => console.log(err))
+  http
+    .get("http://localhost:3000/posts")
+    .then((data) => ui.showPosts(data))
+    .catch((err) => console.log(err));
+}
+
+// Add new post, using http model to make post request
+function submitPost() {
+  const title = document.querySelector("#title").value;
+  const body = document.querySelector("#body").value;
+  // Object literal for values from form
+  const data = {
+    title,
+    body,
+  };
+  // Create post
+  http
+    .post("http://localhost:3000/posts", data)
+    .then((data) => {
+      //
+      ui.showAlert('Post added', 'alert alert-success')
+      ui.clearFields()
+      getPosts(); // this calls all posts, including the new one, for display
+    })
+    .catch((err) => console.log(err))
 }
--- microposts/src/ui.js
+++ microposts/src/ui.js
@@ -9,7 +9,7 @@ class UI {
   }
 
   showPosts(posts) {
-    let output = ''
+    let output = "";
     posts.forEach((post) => {
       output += `
       <div class="card mb-3">
@@ -23,12 +23,46 @@ class UI {
           <i class="fa fa-remove"></i>
           </a>
         </div>
-      </div>`
-    })
+      </div>`;
+    });
 
-    this.post.innerHTML = output
+    this.post.innerHTML = output;
+  }
+  showAlert(message, className) {
+    this.clearAlert()
+
+    // create div
+    const div = document.createElement('div')
+    // add classes
+    div.className = className
+    // add text
+    div.appendChild(document.createTextNode(message))
+    // get parent to insert this in the DOM
+    const container = document.querySelector('.postsContainer')
+    
+    // Get posts
+    const posts = document.querySelector('#posts')
+    // Insert alert div
+    container.insertBefore(div, posts)
+
+    // Timeout
+    setTimeout(() => {
+      this.clearAlert()
+    }, 3000)
+  }
+
+  clearAlert() {
+    const currentAlert = document.querySelector('.alert')
+
+    if(currentAlert) {
+      currentAlert.remove()
+    }
+  }
+
+  clearFields() {
+    this.titleInput = "";
+    this.bodyInput = "";
   }
 }
 
-
-export const ui = new UI()
+export const ui = new UI();
